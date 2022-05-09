fb8890ff8f1aede66225317853faaf442ee9ddc1
jangkyoungtae <zxs3710@naver.com>
2022-05-09 23:36:17 +0900

    GNB 별 화면이동

--- app.py
+++ app.py
@@ -13,5 +13,17 @@ def main():
     myname = "Devom"
     return render_template("index.html", name=myname)
 
+@app.route('/like_list')
+def like_list():
+    return render_template("like_list.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/post_content')
+def profile():
+    return render_template("post_content.html")
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
